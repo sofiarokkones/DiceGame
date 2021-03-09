12c41d09233aef997b6ebcc04009e2a45ede56c8
Sofia Rokkones <72433971+sofiarokkones@users.noreply.github.com>
2021-03-09 09:30:49 +0100

    Add files via upload

--- DiceGameTest/TestDiceGame.cs
+++ DiceGameTest/TestDiceGame.cs
@@ -6,67 +6,67 @@ namespace DiceGameTest
     [TestClass]
     public class TestDiceGame
     {
-        private IDiceGame _diceGame;
+        private IDiceGame diceGame;
 
         [TestInitialize]
         public void Initialize()
         {
-            _diceGame = new DiceGamerHigherOrLower_SixSidedDice();
+            diceGame = new DiceGamerHigherOrLower(6);
         }
 
         [TestMethod]
         public void Dice_Roll_Obtain_Correct_Nbr()
         {
-            int roll = _diceGame.RollDice();
+            int roll = diceGame.RollDice();
             Assert.IsTrue(roll < 7 && roll > 0);
         }
 
         [TestMethod]
         public void Higher_Guess_Is_Correct()
         {
-            Assert.IsTrue(_diceGame.CheckGuess("higher", 5, 2));
+            Assert.IsTrue(diceGame.CheckGuess("higher", 5, 2));
         }
 
         [TestMethod]
         public void Lower_Guess_Is_Correct()
         {
-            Assert.IsTrue(_diceGame.CheckGuess("lower", 2, 5));
+            Assert.IsTrue(diceGame.CheckGuess("lower", 2, 5));
         }
 
         [TestMethod]
         public void Higher_Guess_Is_InCorrect()
         {
-            Assert.IsFalse(_diceGame.CheckGuess("higher", 2, 5));
+            Assert.IsFalse(diceGame.CheckGuess("higher", 2, 5));
         }
 
         [TestMethod]
         public void Lower_Guess_Is_InCorrect()
         {
-            Assert.IsFalse(_diceGame.CheckGuess("lower", 5, 2));
+            Assert.IsFalse(diceGame.CheckGuess("lower", 5, 2));
         }
 
         [TestMethod]
         public void PointCounter_Counts_Correct_RoundOne()
         {
-            Assert.AreEqual(1, _diceGame.PointCounter(0, 1));
+            Assert.AreEqual(1, diceGame.PointCounter(0, 1));
         }
 
         [TestMethod]
         public void PointCounter_Counts_Correct_RoundThree()
         {
-            Assert.AreEqual(5, _diceGame.PointCounter(2, 3));
+            Assert.AreEqual(5, diceGame.PointCounter(2, 3));
         }
 
         [TestMethod]
         public void PointCounter_Counts_Correct_RoundSix()
         {
-            Assert.AreEqual(10, _diceGame.PointCounter(7, 6));
+            Assert.AreEqual(10, diceGame.PointCounter(7, 6));
         }
 
         [TestMethod]
         public void PointCounter_Counts_Correct_RoundSeven()
         {
-            Assert.AreEqual(11, _diceGame.PointCounter(10, 7));
+            Assert.AreEqual(11, diceGame.PointCounter(10, 7));
         }
     }
 }
